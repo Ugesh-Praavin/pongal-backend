bcb776644d3b201a67cf123fa7a530af99371bef
ugeshpraavin <ugesh.240161@csbs.ritchennai.edu.in>
2026-01-01 13:29:28 +0530

    added open port

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
